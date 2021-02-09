e7cc8fa0c5946cfa809c0708aa48156c75ea27b9
Aksel Vaaler <akselva@ntnu.no>
2021-02-09 15:22:04 +0100

    shared variable go complete

--- 2 - shared variable/go/foo.go
+++ 2 - shared variable/go/foo.go
@@ -13,33 +13,44 @@ func number_server(add <-chan int, sub <-chan int, read chan<- int) {
 	// This for-select pattern is one you will become familiar with...
 	for {
 		select {
-        }
+		case <-add:
+			number++
+		case <-sub:
+			number--
+		case read <- number:
+			read <- number
+		}
 	}
 }
 
-func incrementer(add chan<- int, finished chan<- bool) {
+func incrementer(add chan<- int, finishedIncr chan<- bool) {
 	for j := 0; j < 1000000; j++ {
 		add <- 1
 	}
-	//TODO: signal that the goroutine is finished
+	finishedIncr <- true
 }
 
-func decrementer(sub chan<- int, finished chan<- bool) {
+func decrementer(sub chan<- int, finishedDecr chan<- bool) {
 	for j := 0; j < 1000000+1; j++ {
 		sub <- 1
 	}
-	//TODO: signal that the goroutine is finished
+	finishedDecr <- true
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
-	// TODO: Construct the remaining channels
+	add := make(chan int)
+	sub := make(chan int)
+	finishedIncr := make(chan bool)
+	finishedDecr := make(chan bool)
 	read := make(chan int)
 
-	// TODO: Spawn the required goroutines
-
-	// TODO: block on finished from both "worker" goroutines
+	go number_server(add, sub, read)
+	go incrementer(add, finishedIncr)
+	go decrementer(sub, finishedDecr)
+	<-finishedIncr
+	<-finishedDecr
 
 	fmt.Println("The magic number is:", <-read)
 }
