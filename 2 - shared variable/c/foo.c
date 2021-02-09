a2f14074febcacc91961a8ee7e80c00e198410d7
Aksel Vaaler <akselva@ntnu.no>
2021-02-09 15:20:33 +0100

    shared variable c complete

--- 2 - shared variable/c/foo.c
+++ 2 - shared variable/c/foo.c
@@ -2,20 +2,22 @@
 #include <stdio.h>
 
 int i = 0;
-
+pthread_mutex_t mtx;
 // Note the return type: void*
 void* incrementingThreadFunction(){
     for (int j = 0; j < 1000000; j++) {
-	// TODO: sync access to i
+	pthread_mutex_lock(&mtx);
 	i++;
+    pthread_mutex_unlock(&mtx);
     }
     return NULL;
 }
 
 void* decrementingThreadFunction(){
     for (int j = 0; j < 1000000; j++) {
-	// TODO: sync access to i
+	pthread_mutex_lock(&mtx);
 	i--;
+    pthread_mutex_unlock(&mtx);
     }
     return NULL;
 }
@@ -23,13 +25,13 @@ void* decrementingThreadFunction(){
 
 int main(){
     pthread_t incrementingThread, decrementingThread;
-    
+    pthread_mutex_init(&mtx,NULL);
     pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
     pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
     
     pthread_join(incrementingThread, NULL);
     pthread_join(decrementingThread, NULL);
-    
+    pthread_mutex_init(&mtx,NULL);
     printf("The magic number is: %d\n", i);
     return 0;
 }
